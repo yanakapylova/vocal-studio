bafff46bcab957834fbc5720926aba089790114e
yanakapylova <yana.kapylova@innowise.com>
2024-10-21 14:09:53 +0200

    cache module registered

--- server/src/groups/groups.module.ts
+++ server/src/groups/groups.module.ts
@@ -4,6 +4,23 @@ import { GroupsController } from './groups.controller';
 import { PrismaService } from 'prisma/prisma.service';
 
 @Module({
+  imports: [
+    CacheModule.registerAsync<CacheModuleOptions>({
+      useFactory: async () => {
+        const client = createClient({
+          url: 'redis://localhost:6379',
+        });
+        await client.connect();
+
+        return {
+          store: redisStore,
+          url: 'redis://localhost:6379',
+          ttl: 600,
+          isGlobal: true,
+        };
+      },
+    }),
+  ],
   controllers: [GroupsController],
   providers: [GroupsService, PrismaService],
 })
