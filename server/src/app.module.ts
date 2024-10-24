95b5d996f4c2d49890f3189b4c5e0428aac3c316
yanakapylova <yana.kapylova@innowise.com>
2024-10-24 12:24:24 +0200

    pre-fixes

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ScheduleModule } from './schedule/schedule.module';
 import { GroupsModule } from './groups/groups.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, ScheduleModule, GroupsModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, ScheduleModule, GroupsModule, AuthModule]
 })
 export class AppModule {}
